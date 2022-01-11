3fcaf835b01d0b9ce31143b507f03f4a0063b415
mrz1836 <mrz1818@gmail.com>
2022-01-11 08:36:30 -0500

    Bumped version, updated linter

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ import "strings"
 const (
 
 	// version is the current package version
-	version = "v0.1.12"
+	version = "v0.1.13"
 
 	// defaultUserAgent is the default user agent for all requests
 	defaultUserAgent string = "go-bsvrates: " + version
