02f84eb4cd96cb39f7bf15d95d115c4625d34268
mrz1836 <mrz1818@gmail.com>
2022-01-24 16:35:07 -0500

    Upgraded deps, broke apart client interface

--- interface.go
+++ interface.go
@@ -7,11 +7,16 @@ import (
 	"github.com/mrz1836/go-whatsonchain"
 )
 
-// ClientInterface is the BSVRate client interface
-type ClientInterface interface {
-	CoinPaprika() CoinPaprikaInterface
+// RateService is the rate methods
+type RateService interface {
 	GetConversion(ctx context.Context, currency Currency, amount float64) (satoshis int64, providerUsed Provider, err error)
 	GetRate(ctx context.Context, currency Currency) (rate float64, providerUsed Provider, err error)
+}
+
+// ClientInterface is the BSVRate client interface
+type ClientInterface interface {
+	RateService
+	CoinPaprika() CoinPaprikaInterface
 	Preev() preev.ClientInterface
 	Providers() []Provider
 	SetCoinPaprika(client CoinPaprikaInterface)
